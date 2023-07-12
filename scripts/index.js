eca1eed8fee345ccc78b1a26c2031a393c8b3531
Muhammad Hussaini <hussainimuhammad999@gmail.com>
2023-07-12 22:44:47 +0530

    Light/Dark Mode + Some data

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,7 @@ function toggleMenu() {
 
 function toggleMode() {
     document.querySelector('header').classList.toggle("light")
+    document.querySelector('.container-main').classList.toggle("light");
+    document.querySelector('.big-text').classList.toggle("light");
+
 }
