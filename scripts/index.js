27b0d8ceed35c94d2e22bd142254f4396334d706
Muhammad Hussaini <hussainimuhammad999@gmail.com>
2023-07-12 20:13:41 +0530

    Added Navbar

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.querySelector(".hamburger").classList.toggle("opened");
+    document.querySelector("nav").classList.toggle("slided");
+}
+
+function toggleMode() {
+    document.querySelector('header').classList.toggle("light")
+}
