9a04d6ac0997fdfb16ed37857e2f1e4c1dfa80dc
Muhammad Hussaini <hussainimuhammad999@gmail.com>
2023-07-18 23:10:37 +0530

    Added a card

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,6 @@ s.reveal(".p-wrapper .h1", { distance: '200px', origin: 'bottom', duration: 1000
 s.reveal("header", { delay: 2500 })
 s.reveal(".name", { origin: 'left', distance: '100px', duration: 1000, delay: 2500 })
 s.reveal("nav", { origin: 'top', distance: '100px', duration: 1000, delay: 2500 })
+s.reveal(".left-1", {origin: 'left', distance: '50%', duration: 1000, delay: 500})
+s.reveal(".right-1 h1", {origin: 'top', distance: '50px', duration: 1000, delay: 800})
+s.reveal(".right-1 a", {origin: 'bottom', distance: '50px', duration: 1000, delay: 800})
