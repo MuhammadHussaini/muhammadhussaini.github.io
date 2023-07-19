1347bb2d47b167629d9acfe5795b3925109eb2db
Muhammad Hussaini <hussainimuhammad999@gmail.com>
2023-07-19 22:15:12 +0530

    Added flappy bird project

--- scripts/desmos.js
+++ scripts/desmos.js
@@ -9,4 +9,9 @@ function scrollReveal() {
     s.reveal(".left", { origin: 'left', distance: '50%', duration: 1500 })
     s.reveal(".right", { origin: 'right', distance: '50%', duration: 1500 })
     s.reveal("header", { duration: 2500, afterReveal: () => { document.querySelector('header').style.transition = '0.7s' } })
+    s.reveal(".left-1", { origin: 'left', distance: '50%', duration: 1500 })
+    s.reveal(".right-1", { origin: 'right', distance: '50%', duration: 1500 })
+    s.reveal(".left-2", { origin: 'left', distance: '50%', duration: 1500 })
+    s.reveal(".right-2", { origin: 'right', distance: '50%', duration: 1500 })
+    s.reveal(".bar", {delay: 1200})
 }
